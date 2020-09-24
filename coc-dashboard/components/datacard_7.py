183d1cbc9fee94abd491a4ce3fc5ba510e7a7467
nbigaud <nathan.bigaud@dalberg.com>
2020-09-24 07:21:11 +0000

    Modification of kwargs f data functions

--- coc-dashboard/components/datacard_7.py
+++ coc-dashboard/components/datacard_7.py
@@ -7,7 +7,7 @@ from helpers import (filter_df_by_policy,
 
 
 # @timeit
-def scatter_reporting_district_data(dfs, indicator, district, **kwargs):
+def scatter_reporting_district_data(dfs, static, indicator, district, **kwargs):
 
     df_reporting = filter_df_by_policy(dfs, 'Reporting')
 
