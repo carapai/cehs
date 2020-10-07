542b83faf64bae68367306459fc3fb4b7519eefa
nbigaud <nathan.bigaud@dalberg.com>
2020-10-07 17:30:54 +0000

    Added load_dotenv

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 # from COC_dashboard import data_story
 # from demo_v1 import data_story
 
+
 import sys
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
 
 sys.path.insert(0, "./coc-dashboard")
 
+from callbacks import define_callbacks  # NOQA: E402
 from view import ds  # NOQA: E402
 
 server = ds.app.server
 
-from callbacks import define_callbacks
 
 define_callbacks(ds)
 
 if __name__ == "__main__":
-    ds.run(dev=True)
+    ds.run(dev=False)
 else:
     ds.set_layout_and_callbacks()
