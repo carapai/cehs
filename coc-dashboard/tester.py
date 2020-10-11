b0c96361a8f03c99d6d40f37480000e97f5e9bf7
nbigaud <nathan.bigaud@dalberg.com>
2020-10-11 03:04:08 +0000

    Mid changes to update dropdowns

--- coc-dashboard/tester.py
+++ coc-dashboard/tester.py
@@ -1,4 +1,12 @@
-a = 'EPI (coverage)'
-b = 'EPI'
+import sys
+from package.components.nested_dropdown_group import NestedDropdownGroup
 
-print(a.split('(')[-1][:-1], b.split('(')[-1][:-1])
+sys.path.insert(0, "./coc-dashboard")
+
+indicator_group = pd.read_csv("./coc-dashboard/data/groups.csv")
+
+indicator_dropdown_group = NestedDropdownGroup(
+    indicator_group, title="Select an indicator"
+)
+
+print(indicator_dropdown_group.dropdown_ids)
